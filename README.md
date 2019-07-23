71101b898d57eeb988b9e18a5221ce2b0654d2c5
Qxf2 <mak@qxf2.com>
2019-07-23 18:34:12 +0530

    Version 1 of requirements

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # interview-scheduler
-This repo contains code needed for the Qxf2 interview scheduler application.
+This repo contains code needed for the Qxf2 interview scheduler application. Please follow the setup instructions to start using the application.
+
+--------------
+1. MOTIVATION 
+--------------
+
+1. Allow Qxf2 to handle a large volume of interview candidates without having to bother about scheduling
+2. Make the interaction with Qxf2 different from other companies 
+3. Remind candidates at key moments during the process about Qxf2's features
+4. Reduce (almost eliminate) exchange of emails between candidate and interviewer
+
+The interview scheduler application should replace the scheduling work Gracy and Smitha did when we hired Rohini. 
---
+++ utils/verify_gcal_setup.py
@@ -0,0 +1,56 @@
+"""
+This is a simple script to test the Google calendar
+"""
+
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+import sys 
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def main(email_id):
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            print("Your credentials to access Google Calendar are not setup correctly")
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId=email_id, timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+if __name__ == '__main__':
+    if len(sys.argv)>1:
+        email_id = sys.argv[1]
+        main(email_id)
+    else:
+        print("USAGE: %s your@domain.com\nEXAMPLE: %s test@qxf2.com\n"%(__file__,__file__))
+

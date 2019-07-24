46ef02b129a151014926193d6cf0074d8d49dc55
Annapoorani Bala <Arunkumar>
2019-07-24 12:55:41 +0530

    Added the text box for email,date and added the submit button

--- qxf2_scheduler/routes.py
+++ qxf2_scheduler/routes.py
@@ -5,12 +5,28 @@ This file contains all the endpoints exposed by the interview scheduler applicat
 from flask import render_template, url_for, flash, redirect, jsonify, request, Response
 from qxf2_scheduler import app
 
-@app.route("/get-schedule")
+
+@app.route("/get-schedule",methods=['GET','POST'])
 def date_picker():
     "Dummy page to let you see a schedule"
-    return render_template('get-schedule.html')
+    if request.method == 'GET':            
+            return render_template('get-schedule.html')
+    if request.method == 'POST': 
+        print("I am coming here",file=sys.stderr)
+        email=request.form.get('emailfield')
+        date=request.form.get('datefield') 
+        if email and date:        
+                return render_template('free-available-slot.html')
+        
+
+@app.route("/available-free-slot")
+def available_free_slot():
+    "Returns the available free slot"
+    return render_template('free-available-slot.html')
+    
 
 @app.route("/")
 def index():
     "The index page"
-    return "This page is not ready yet!"
+    return "The page is not ready yet"
+    

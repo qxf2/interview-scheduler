d82c845b014587c86dca2b301a60b17816f4e38b
Annapoorani Bala <Arunkumar>
2019-07-24 18:27:49 +0530

    Integrate google calendar with an app

--- qxf2_scheduler/routes.py
+++ qxf2_scheduler/routes.py
@@ -4,29 +4,21 @@ This file contains all the endpoints exposed by the interview scheduler applicat
 
 from flask import render_template, url_for, flash, redirect, jsonify, request, Response
 from qxf2_scheduler import app
-
+import utils.verify_gcal_setup as gcal 
 
 @app.route("/get-schedule",methods=['GET','POST'])
 def date_picker():
     "Dummy page to let you see a schedule"
     if request.method == 'GET':            
             return render_template('get-schedule.html')
-    if request.method == 'POST': 
-        print("I am coming here",file=sys.stderr)
-        email=request.form.get('emailfield')
-        date=request.form.get('datefield') 
-        if email and date:        
-                return render_template('free-available-slot.html')
-        
-
-@app.route("/available-free-slot")
-def available_free_slot():
-    "Returns the available free slot"
-    return render_template('free-available-slot.html')
-    
+    if request.method == 'POST':         
+        email=request.form.get('email')
+        date=request.form.get('date') 
+        events = gcal.main(email)
+        api_response = {"events":events,"email":email}
+        return jsonify(api_response)
 
 @app.route("/")
 def index():
     "The index page"
-    return "The page is not ready yet"
-    
+    return "The page is not ready yet!"    

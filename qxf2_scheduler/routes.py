26f56b04ec01c0bcbc8b9eb5eef6b035e752b297
Annapoorani Bala <Arunkumar>
2019-07-25 19:02:03 +0530

    Pushing the working copy of get the events for a date

--- qxf2_scheduler/routes.py
+++ qxf2_scheduler/routes.py
@@ -5,23 +5,24 @@ This file contains all the endpoints exposed by the interview scheduler applicat
 from flask import render_template, url_for, flash, redirect, jsonify, request, Response
 from qxf2_scheduler import app
 import qxf2_scheduler.base_gcal as gcal
-import datetime,sys
+import datetime
+import sys
+import pytz
 from datetime import timedelta
 
-
-def get_all_events(email_id,fetch_date):
-    "Get all the events for a fetched date" 
-    service = gcal.base_gcal()    
-    start_date = datetime.datetime.strptime(fetch_date,'%Y-%m-%d')
+def get_all_events(email_id, fetch_date):
+    "Get all the events for a fetched date"
+    service = gcal.base_gcal()
+    start_date = datetime.datetime.strptime(fetch_date, '%Y-%m-%d')
     end_date = start_date + timedelta(days=1)
     start_date = start_date.isoformat() + 'Z'
     end_date = end_date.isoformat() + 'Z'
-    
-    #now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+    # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId=email_id, timeMin=start_date,
-                                        maxResults=10, timeMax=end_date,singleEvents=True,
-                                        orderBy='startTime').execute()
+                                          maxResults=10, timeMax=end_date, singleEvents=True,
+                                          orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
@@ -33,19 +34,20 @@ def get_all_events(email_id,fetch_date):
     return events
 
 
-@app.route("/get-schedule",methods=['GET','POST'])
+@app.route("/get-schedule", methods=['GET', 'POST'])
 def date_picker():
     "Dummy page to let you see a schedule"
-    if request.method == 'GET':            
-            return render_template('get-schedule.html')
-    if request.method == 'POST':         
-        email=request.form.get('email')
-        date=request.form.get('date')        
-        all_events = get_all_events(email,date)       
-        api_response = {"events":all_events,"email":email}
+    if request.method == 'GET':
+        return render_template('get-schedule.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        date = request.form.get('date')
+        all_events = get_all_events(email, date)        
+        api_response = {"events": all_events, "email": email}
         return jsonify(api_response)
+
 
 @app.route("/")
 def index():
     "The index page"
-    return "The page is not ready yet!"    
+    return "The page is not ready yet!"

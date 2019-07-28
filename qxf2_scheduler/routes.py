d0eb3f08d0a06adc09027e504b2c9870401cdefe
Qxf2 <mak@qxf2.com>
2019-07-28 21:21:05 +0530

    added a check for domain name and also disabled past dates in the selector

--- qxf2_scheduler/routes.py
+++ qxf2_scheduler/routes.py
@@ -5,6 +5,7 @@ This file contains all the endpoints exposed by the interview scheduler applicat
 from flask import render_template, url_for, flash, redirect, jsonify, request, Response
 from qxf2_scheduler import app
 import qxf2_scheduler.qxf2_scheduler as my_scheduler
+DOMAIN = 'qxf2.com'
 
 @app.route("/get-schedule", methods=['GET', 'POST'])
 def date_picker():
@@ -14,8 +15,11 @@ def date_picker():
     if request.method == 'POST':
         email = request.form.get('email')
         date = request.form.get('date')
-        free_slots = my_scheduler.get_free_slots_for_date(email, date)
-        api_response = {"free_slots": free_slots, "email": email, "date": date}
+        if '@' + DOMAIN == email[-9:]:
+            free_slots = my_scheduler.get_free_slots_for_date(email, date)
+            api_response = {'free_slots': free_slots, 'email': email, 'date': date}
+        else:
+            api_response = {'error':'This application will only work for emails ending in @{domain}'.format(domain=DOMAIN)}
         return jsonify(api_response)
 
 

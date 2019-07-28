9d43298f4e0a40bfaf7a51f79a04e1255aa572cf
Qxf2 <mak@qxf2.com>
2019-07-29 00:10:24 +0530

    Added frontend and backend protection for Qxf2 holidays

--- qxf2_scheduler/routes.py
+++ qxf2_scheduler/routes.py
@@ -19,6 +19,8 @@ def date_picker():
             api_response = {'error':'This application will only work for emails ending in @{domain}'.format(domain=DOMAIN)}
         elif my_scheduler.is_past_date(date):
             api_response = {'error':'You can only get schedules for today or later'}
+        elif my_scheduler.is_qxf2_holiday(date):
+            api_response = {'error':'The date you have provided is a Qxf2 holiday'}
         else:
             free_slots = my_scheduler.get_free_slots_for_date(email, date)
             api_response = {'free_slots': free_slots, 'email': email, 'date': date}

47a0faac148888ec79e2dc3f7ccd62023d0a0987
Qxf2 <mak@qxf2.com>
2019-07-30 16:35:21 +0530

    Fixed the case where an event starts at the day start but ends before it

--- qxf2_scheduler/qxf2_scheduler.py
+++ qxf2_scheduler/qxf2_scheduler.py
@@ -51,7 +51,7 @@ def get_free_slots(busy_slots, day_start, day_end):
             continue 
         elif busy_slot['start'] <= day_start and busy_slot['end'] >= day_end:
             break 
-        elif busy_slot['start'] < day_start and busy_slot['end'] < day_end:
+        elif busy_slot['start'] <= day_start and busy_slot['end'] < day_end:
             free_slots.append(busy_slot['end'])
         elif busy_slot['start'] > day_start and busy_slot['end'] > day_end:
             free_slots.append(busy_slot['start'])

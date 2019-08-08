df3baef22dc7dec519fa58118aee3587a0b1dd17
Annapoorani Bala <Arunkumar>
2019-08-08 19:07:11 +0530

    Added the interviewer column field

--- qxf2_scheduler/models.py
+++ qxf2_scheduler/models.py
@@ -1,10 +1,11 @@
 from qxf2_scheduler import db
 
 class Addinterviewer(db.Model):
-    "Adding the interviewer"    
-    interviewer_name = db.column(db.string(10),nullable=False)
-    interviewer_email = db.column(db.string(30),nullable=False)
-    interviewer_designation = db.column(db.string(10),nullable=False)
+    "Adding the interviewer" 
+    interviewer_id = db.Column(db.Integer(10),primary_key=True,nullable=False)   
+    interviewer_name = db.Column(db.String(50),nullable=False)
+    interviewer_email = db.Column(db.String(50),nullable=False)
+    interviewer_designation = db.Column(db.String(40),nullable=False)
     
     def __repr__(self):
         return f"Addinterviewer('{self.interviewer_name}', '{self.interviewer_email}','{self.interviewer_designation}'')"

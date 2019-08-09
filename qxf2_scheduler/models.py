7570f96d01601471bb06083a168fdd4a6cf3eec3
Annapoorani Bala <Arunkumar>
2019-08-09 15:29:27 +0530

    Added the class for adding interviewer

--- qxf2_scheduler/models.py
+++ qxf2_scheduler/models.py
@@ -1,17 +1,12 @@
 from qxf2_scheduler import db
 
+
 class Addinterviewer(db.Model):
     "Adding the interviewer" 
-    interviewer_id = db.Column(db.Integer(10),primary_key=True,nullable=False)   
+    interviewer_id = db.Column(db.Integer,primary_key=True,nullable=False)   
     interviewer_name = db.Column(db.String(50),nullable=False)
     interviewer_email = db.Column(db.String(50),nullable=False)
     interviewer_designation = db.Column(db.String(40),nullable=False)
     
     def __repr__(self):
         return f"Addinterviewer('{self.interviewer_name}', '{self.interviewer_email}','{self.interviewer_designation}'')"
-
-
-class Intervieweravailability(db.model):
-    "Adding the interviewer availability"
-    person_id = db.Column(db.Integer(10), db.ForeignKey('Addinterviewer.interviewer_id'),nullable=False)
-    time_availability = db.Column(db.Integer(10),nullable=False)

594b8ce8c4a115f2b8c2f1d3c0fc03e9f0e95431
Annapoorani Bala <Arunkumar>
2019-08-09 16:57:20 +0530

    Added the class for interviewer time availability

--- qxf2_scheduler/models.py
+++ qxf2_scheduler/models.py
@@ -1,5 +1,5 @@
 from qxf2_scheduler import db
-
+from sqlalchemy import Integer, ForeignKey, String, Column
 
 class Addinterviewer(db.Model):
     "Adding the interviewer" 
@@ -10,3 +10,9 @@ class Addinterviewer(db.Model):
     
     def __repr__(self):
         return f"Addinterviewer('{self.interviewer_name}', '{self.interviewer_email}','{self.interviewer_designation}'')"
+
+class Intervieweravailability(db.Model):
+    "Adding the timing for interviewer" 
+    time_availability = db.Column(db.String(20),nullable=False)
+    person_id = db.Column(db.Integer,primary_key=True,nullable=False)
+    interviewer_id = db.Column(db.Integer,ForeignKey(Addinterviewer.interviewer_id))

c2bda82250cb552a3567a7838879eddd3115b7c3
Annapoorani Bala <Arunkumar>
2019-08-14 22:20:22 +0530

    Changed the table name

--- qxf2_scheduler/models.py
+++ qxf2_scheduler/models.py
@@ -1,7 +1,7 @@
 from qxf2_scheduler import db
 from sqlalchemy import Integer, ForeignKey, String, Column
 
-class Addinterviewer(db.Model):
+class Interviewers(db.Model):
     "Adding the interviewer" 
     interviewer_id = db.Column(db.Integer,primary_key=True,nullable=False)   
     interviewer_name = db.Column(db.String(50),nullable=False)
@@ -9,10 +9,15 @@ class Addinterviewer(db.Model):
     interviewer_designation = db.Column(db.String(40),nullable=False)
     
     def __repr__(self):
-        return f"Addinterviewer('{self.interviewer_name}', '{self.interviewer_email}','{self.interviewer_designation}'')"
+        return f"Interviewers('{self.interviewer_name}', '{self.interviewer_email}','{self.interviewer_designation}'')"
 
-class Intervieweravailability(db.Model):
-    "Adding the timing for interviewer" 
-    time_availability = db.Column(db.String(20),nullable=False)
-    person_id = db.Column(db.Integer,primary_key=True,nullable=False)
-    interviewer_id = db.Column(db.Integer,ForeignKey(Addinterviewer.interviewer_id))
+class Interviewertimeslots(db.Model):
+    "Adding the timing for interviewer"    
+    interviewer_id = db.Column(db.Integer,ForeignKey(Interviewers.interviewer_id),nullable=False)
+    time_id = db.Column(db.Integer,primary_key=True)
+    interviewer_start_time = db.Column(db.String,nullable=False)
+    interviewer_end_time = db.Column(db.String,nullable=False)
+
+    def __repr__(self):
+        return f"Interviewertimeslots('{self.interviewer_id}', '{self.interviewer_start_time}','{self.interviewer_end_time}','{self.time_id}'')"
+

22503692018d89f0452c25fb2d0bb0a425e2e756
akkuldn <akkul.dn@qxf2.com>
2019-08-21 16:06:47 +0530

    Code implemented to check wether the free slots displayed in the app is accurate

---
+++ QA/conf/fetch_api_interviewscheduler_conf.py
@@ -0,0 +1,6 @@
+"""
+This page provides configuration details to provide in order to fetch the api response
+"""
+emailid="akkul.dn@qxf2.com"
+date="10/07/2025"
+url="http://127.0.0.1:6464/get-schedule"
---
+++ QA/conf/google_api_conf.py
@@ -0,0 +1,7 @@
+"""
+This file contains the configuration details to fetch events from google api
+"""
+email_id="test@qxf2.com"
+mintime="2025-07-10T09:00:00+05:30"
+maxtime="2025-07-10T17:00:00+05:30"
+date="10/07/2025"

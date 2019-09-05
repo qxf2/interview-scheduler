55fb53ab4262dc2e41087ef8728fced7f230c4c4
kirancv4 <kiran.cv@qxf2.com>
2019-09-05 11:42:45 +0530

    Added files for database migration script

---
+++ migrate_db.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from qxf2_scheduler.models import Interviewers,Interviewertimeslots
+from qxf2_scheduler import db
+from qxf2_scheduler.__init__ import app
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+
+migrate=Migrate(app, db)
+manager=Manager(app)
+
+manager.add_command('db',MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()

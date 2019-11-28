1b9a6d81795d366548856cf32e911be41bc0b047
Annapoorani Bala <Arunkumar>
2019-11-28 12:56:04 +0530

    Changed the string format in models.py

--- qxf2_scheduler/models.py
+++ qxf2_scheduler/models.py
@@ -42,9 +42,9 @@ class Jobinterviewer(db.Model):
 class Candidates(db.Model):
     "Adding the candidates"
     candidate_id = db.Column(db.Integer,primary_key=True)
-    candidate_name = db.Column(db.Integer,nullable=False)
-    candidate_email = db.Column(db.Integer,nullable=False)
-    job_applied = db.Column(db.Integer,nullable=False)
+    candidate_name = db.Column(db.String,nullable=False)
+    candidate_email = db.Column(db.String,nullable=False)
+    job_applied = db.Column(db.String,nullable=False)
 
     def __repr__(self):
         return f"Candidates('{self.candidate_name}','{self.candidate_email}','{self.job_applied}')"

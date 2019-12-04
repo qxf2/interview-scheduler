f0b4df096de2e664fc29987b484c43a644c8f148
monoranjanqxf2 <monoranjan.mandal@qxf2.com>
2019-12-04 19:02:22 +0530

    editted entire function

--- qxf2_scheduler/candidates.py
+++ qxf2_scheduler/candidates.py
@@ -10,10 +10,10 @@ DOMAIN = 'qxf2.com'
 @app.route("/candidates",methods=["GET"])
 def read_candidates():
     "Read the candidates"      
-    display_candidates = Candidates.query.all()
+    display_candidates = db.session.query(Candidates, Jobs).filter(Candidates.job_id == Jobs.job_id).values(Candidates.candidate_id,Candidates.candidate_name,Candidates.candidate_email,Candidates.job_id,Jobs.job_role)
     my_candidates_list = []
     for each_candidate in display_candidates:
-        my_candidates_list.append({'candidate_id':each_candidate.candidate_id,'candidate_name':each_candidate.candidate_name,'candidate_email':each_candidate.candidate_email,'job_applied':each_candidate.job_applied})
+        my_candidates_list.append({'candidate_id':each_candidate.candidate_id,'candidate_name':each_candidate.candidate_name,'candidate_email':each_candidate.candidate_email,'job_id':each_candidate.job_id,'job_role':each_candidate.job_role})
         
     return render_template("read-candidates.html",result=my_candidates_list)
 
@@ -37,6 +37,7 @@ def delete_candidate():
 def add_candidate(job_role):
     "Add a candidate"
     data,error = [],None
+    job_available = Jobs.query.all()
     if request.method == 'GET':
         available_job_list = []        
         if job_role is None:
@@ -52,17 +53,33 @@ def add_candidate(job_role):
 
     if request.method == 'POST':        
         candidate_name = request.form.get('candidateName')
-        candidate_email = request.form.get('candidateEmail')
-        candidate_job_applied = request.form.get('jobApplied')        
+        candidate_email = request.form.get('candidateEmail').lower()
+        candidate_job_applied = request.form.get('jobApplied')  
+        #job_available =  Jobs.query.all()
+        for each_job in job_available:
+            if each_job.job_role == candidate_job_applied:
+                job_id = each_job.job_id    
         data = {'candidate_name':candidate_name}
         #Check the candidate has been already added or not
         check_candidate_exists = db.session.query(db.exists().where(Candidates.candidate_email==candidate_email)).scalar()        
         if check_candidate_exists == True:
             error = "The user already exists in the table"            
         else:
-            add_candidate_object = Candidates(candidate_name=candidate_name,candidate_email=candidate_email,job_applied=candidate_job_applied)
+            error = "The successfully added" 
+            add_candidate_object = Candidates(candidate_name=candidate_name,candidate_email=candidate_email,job_id=job_id)
             db.session.add(add_candidate_object)
             db.session.commit()
 
         api_response = {'data':data,'error':error}
         return jsonify(api_response)
+
+
+        
+@app.route("/candidate/<job_id>/<candidate_id>") 
+def show_candidate_job(job_id,candidate_id):
+    "Show candidate name and job role"     
+    candidate_job_data = Candidates.query.join(Jobs, Candidates.job_id == Jobs.job_id).filter(Candidates.candidate_id == candidate_id).values(Candidates.candidate_name, Jobs.job_role)
+    for each_data in candidate_job_data:
+        data = {'candidate_name':each_data.candidate_name,'job_applied':each_data.job_role} 
+ 
+    return render_template("candidate-job-status.html",result=data)

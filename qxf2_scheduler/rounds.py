2e0dbd2a2ac1c6d39a20216447e2f7b4bfc76eee
Annapoorani Bala <Arunkumar>
2019-12-13 16:24:54 +0530

    Added the front end for add rounds

--- qxf2_scheduler/rounds.py
+++ qxf2_scheduler/rounds.py
@@ -6,11 +6,23 @@ import json,ast,sys
 
 from qxf2_scheduler.models import Jobs, Rounds
 
-@app.route("/job/<job_id>/rounds")
+@app.route("/job/<job_id>/rounds",methods=["GET","POST"])
 def read_round_details(job_id):
     "read round details"
-    round_details = db.session.query(Rounds).values(Rounds.round_time,Rounds.round_description,Rounds.round_requirement)
-    for each_round in round_details:
-        data = {'round_time':each_round.round_time,'round_description':each_round.round_description,'round_requirements':each_round.round_requirement}
-    print("data",data)
-    return render_template("rounds.html",result=data)
+    if request.method == 'GET':
+        round_details = db.session.query(Rounds).values(Rounds.round_time,Rounds.round_description,Rounds.round_requirement)
+        for each_round in round_details:
+            data = {'job_id':job_id,'round_time':each_round.round_time,'round_description':each_round.round_description,'round_requirements':each_round.round_requirement}
+        print("data",data)
+    
+        return render_template("rounds.html",result=data)
+
+    if request.method == "POST":
+        print("I am coming here",file=sys.stderr)
+        round_time = request.form.get('duration')
+        round_description = request.form.get('round_description')
+        round_requirements = request.form.get('round_requirements')
+        add_round_object = Rounds({'round_time':round_time,'round_description':round_description,'round_requirement':round_requirements})
+        db.session.add(add_round_object)
+        db.session.commit()
+

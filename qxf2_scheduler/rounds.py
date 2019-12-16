635b230b5be494ffc654555d7933dccd4761f1b8
Annapoorani Bala <Arunkumar>
2019-12-16 16:07:36 +0530

    Handled the empty dictionary value

--- qxf2_scheduler/rounds.py
+++ qxf2_scheduler/rounds.py
@@ -9,8 +9,9 @@ from qxf2_scheduler.models import Jobs, Rounds
 @app.route("/job/<job_id>/rounds")
 def read_round_details(job_id):
     "read round details"
+    data = {}
     round_details = db.session.query(Rounds).values(Rounds.round_time,Rounds.round_description,Rounds.round_requirement)
     for each_round in round_details:
         data = {'round_time':each_round.round_time,'round_description':each_round.round_description,'round_requirements':each_round.round_requirement}
-    print("data",data)
+    
     return render_template("rounds.html",result=data)

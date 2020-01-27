65d188d8adc994de1addfce4bcfa997c308b608e
Annapoorani Bala <Arunkumar>
2020-01-27 15:08:47 +0530

    Filtered the email iid for the interview

--- qxf2_scheduler/candidates.py
+++ qxf2_scheduler/candidates.py
@@ -155,12 +155,16 @@ def show_candidate_job(job_id,candidate_id):
     round_details = {}     
     candidate_job_data = db.session.query(Jobs, Candidates, Jobcandidate).filter(Candidates.candidate_id == candidate_id,Jobs.job_id == job_id,Jobcandidate.candidate_id == candidate_id,Jobcandidate.job_id == job_id).values(Candidates.candidate_name, Candidates.candidate_email,Candidates.date_applied,Jobs.job_role,Jobs.job_id,Candidates.candidate_id,Jobcandidate.url,Jobcandidate.candidate_status,Jobcandidate.interviewer_email)
     for each_data in candidate_job_data:
-        data = {'candidate_name':each_data.candidate_name,'job_applied':each_data.job_role,'candidate_id':candidate_id,'job_id':job_id,'url': each_data.url,'candidate_email':each_data.candidate_email,'candidate_status':each_data.candidate_status,'interviewer_email_id':each_data.interviewer_email,'date_applied':each_data.date_applied} 
-    #Fetch all the round details for a job
-    round_ids_for_job = Jobround.query.filter(Jobround.job_id==job_id).all()
-    for each_round_id in round_ids_for_job:
-        round_detail = db.session.query(Rounds).filter(Rounds.round_id==each_round_id.round_id).scalar()
-        round_details = {'round_name':round_detail.round_name,'round_id':round_detail.round_id,'round_description':round_detail.round_description}
+        data = {'candidate_name':each_data.candidate_name,'job_applied':each_data.job_role,'candidate_id':candidate_id,'job_id':job_id,'url': each_data.url,'candidate_email':each_data.candidate_email,'interviewer_email_id':each_data.interviewer_email,'date_applied':each_data.date_applied}
+        candidate_status_id = each_data.candidate_status
+    #fetch the candidate status name for the status id
+    candidate_status_name = db.session.query(Candidatestatus).filter(Candidatestatus.status_id==candidate_status_id).scalar()
+    data['candidate_status']=candidate_status_name.status_name
+    pending_round_ids = get_pending_round_id(job_id,candidate_id)
+    #Get the pending round id details from the table
+    for each_round_id in pending_round_ids:
+        round_detail = db.session.query(Rounds).filter(Rounds.round_id==each_round_id).scalar()
+        round_details = {'round_name':round_detail.round_name,'round_id':round_detail.round_id,'round_description':round_detail.round_description,'round_time':round_detail.round_time}
         round_names_list.append(round_details)
     return render_template("candidate-job-status.html",result=data,round_names=round_names_list)
 

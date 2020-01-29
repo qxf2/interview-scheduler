c9c2262c128eaafdb8e3913b71c3fc92cc75f59c
Annapoorani Bala <Arunkumar>
2020-01-29 19:09:53 +0530

    Added the delete method for status

--- qxf2_scheduler/status.py
+++ qxf2_scheduler/status.py
@@ -18,3 +18,17 @@ def read_status():
         my_status_list.append({'status_id':each_status.status_id,'status_name':each_status.status_name})
     
     return render_template("read-status.html",result=my_status_list)
+
+
+@app.route("/status/<status_id>/delete",methods=["POST"]) 
+def delete_status(status_id):
+    "Deletes a candidate"
+    if request.method == 'POST':
+        status_id_to_delete = request.form.get('statusid')        
+        #Delete the status from status table
+        status_to_delete = Candidatestatus.query.filter(Candidatestatus.status_id==status_id_to_delete).first()
+        data = {'status_name':status_to_delete.status_name,'status_id':status_to_delete.status_id}       
+        db.session.delete(status_to_delete)
+        db.session.commit() 
+                
+    return jsonify(data)

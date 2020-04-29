95dc956a30dd1d1adcd0272ceb38ab32e79674d2
Annapoorani Bala <Arunkumar>
2020-04-29 19:39:07 +0530

    Added the interview details in the candidate page

--- qxf2_scheduler/candidates.py
+++ qxf2_scheduler/candidates.py
@@ -283,13 +283,22 @@ def show_candidate_job(job_id,candidate_id):
     "Show candidate name and job role"
     round_names_list = []
     round_details = {}     
-    candidate_job_data = db.session.query(Jobs, Candidates, Jobcandidate).filter(Candidates.candidate_id == candidate_id,Jobs.job_id == job_id,Jobcandidate.candidate_id == candidate_id,Jobcandidate.job_id == job_id).values(Candidates.candidate_name, Candidates.candidate_email,Candidates.date_applied,Jobs.job_role,Jobs.job_id,Candidates.candidate_id,Jobcandidate.url,Jobcandidate.candidate_status,Jobcandidate.interviewer_email,Jobcandidate.url,Candidates.comments)
+    candidate_job_data = db.session.query(Jobs, Candidates, Jobcandidate).filter(Candidates.candidate_id == candidate_id,Jobs.job_id == job_id,Jobcandidate.candidate_id == candidate_id,Jobcandidate.job_id == job_id).values(Candidates.candidate_name, Candidates.candidate_email,Candidates.date_applied,Jobs.job_role,Jobs.job_id,Candidates.candidate_id,Jobcandidate.url,Jobcandidate.candidate_status,Jobcandidate.interviewer_email,Jobcandidate.url,Candidates.comments,Jobcandidate.interview_start_time,Jobcandidate.interview_date)
     for each_data in candidate_job_data:
         if each_data.url == '':
             url = None
         else:
             url = base_url + each_data.url + '/welcome'
-        data = {'candidate_name':each_data.candidate_name,'job_applied':each_data.job_role,'candidate_id':candidate_id,'job_id':job_id,'url': each_data.url,'candidate_email':each_data.candidate_email,'interviewer_email_id':each_data.interviewer_email,'date_applied':each_data.date_applied.date(),'url':url,'comments':each_data.comments}         
+        if (each_data.interview_date == None):
+            interview_date = None
+            interview_start_time = None
+        else:
+            interview_date = each_data.interview_date
+            interview_start_time = each_data.interview_start_time
+            interview_start_time = datetime.datetime.strptime(interview_start_time,'%Y-%m-%dT%H:%M:%S+05:30')
+            interview_start_time = interview_start_time.time()
+
+        data = {'candidate_name':each_data.candidate_name,'job_applied':each_data.job_role,'candidate_id':candidate_id,'job_id':job_id,'url': each_data.url,'candidate_email':each_data.candidate_email,'interviewer_email_id':each_data.interviewer_email,'date_applied':each_data.date_applied.date(),'url':url,'comments':each_data.comments,'interview_date':interview_date,'interview_start_time':interview_start_time}         
         candidate_status_id = each_data.candidate_status
     #fetch the candidate status name for the status id
     candidate_status_name = db.session.query(Candidatestatus).filter(Candidatestatus.status_id==candidate_status_id).scalar()

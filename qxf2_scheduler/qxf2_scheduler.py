41c9942506fe3fe30a5d8de5deffdb818d0240d8
Annapoorani Bala <Arunkumar>
2020-07-21 18:17:33 +0530

    fixed the max one interview

--- qxf2_scheduler/qxf2_scheduler.py
+++ qxf2_scheduler/qxf2_scheduler.py
@@ -385,7 +385,6 @@ def get_busy_slots_for_date(email_id,fetch_date,debug=False):
     "Get the busy slots for a given date"
     service = gcal.base_gcal()
     busy_slots = []
-    fetch_date = '07/06/2020'
     if service:
         try:
             all_events = gcal.get_events_for_date(service,email_id,fetch_date)

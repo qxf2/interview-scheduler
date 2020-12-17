9bd36732cb366e9b718e201e6174af64c694c029
Annapoorani Bala <Arunkumar>
2020-12-17 12:35:52 +0530

    Added the print statement

--- tests/test_select_interviewer.py
+++ tests/test_select_interviewer.py
@@ -8,16 +8,16 @@ from qxf2_scheduler.qxf2_scheduler import total_busy_slots
 class pick_interviewer(unittest.TestCase):
 
     date='12/22/2020'
+    print("I am in 11")
 
+    @mock.patch.object(total_busy_slots,'attendee_email_id',return_value=['namitha.sathyananda@qxf2.com', 'nilaya@qxf2.com', 'dennis.samson@qxf2.com'],create=True)
 
-
-    @mock.patch.object(total_busy_slots,'attendee_email_id',return_value=['namitha.sathyananda@qxf2.com', 'nilaya@qxf2.com', 'dennis.samson@qxf2.com'] )
-
-    @mock.patch.object(total_busy_slots,'date',return_value='12/22/2020')
+    @mock.patch.object(total_busy_slots,'date',return_value=parse('12/22/2020'),create=True)
 
     def test_total_busy_slots(self,mockattendee_email_id,mockdate):
-
+        print("I am in 17")
         total_busy_time_list = [datetime.datetime(1900, 1, 1, 1, 15), datetime.datetime(1900, 1, 1, 2, 10), datetime.datetime(1900, 1, 1, 1, 15), datetime.datetime(1900, 1, 1, 1, 25)]
+        print("Failed")
 
         assert total_busy_slots(mockattendee_email_id.return_value,mockdate.return_value)== total_busy_time_list
 

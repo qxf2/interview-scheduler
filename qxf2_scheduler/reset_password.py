c5f96562ba1967f347bd99d81977d61664886583
Annapoorani Bala <Arunkumar>
2020-12-17 10:58:30 +0530

    Added the reset password

---
+++ qxf2_scheduler/reset_password.py
@@ -0,0 +1,13 @@
+from qxf2_scheduler import app
+from flask import render_template
+from qxf2_scheduler import db
+
+from qxf2_scheduler.models import Login
+
+@app.route("/reset-password", methods=["GET","POST"])
+def reset_password():
+    "Reset the password"
+    if request.method == "GET":
+        return render_template("reset-password.html")
+    if request.method == "POST":
+        email_id = request.form.get("emailid")

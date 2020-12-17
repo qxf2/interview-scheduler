fd995828d8d3a64cf32a67f2875e1a6b44d9741d
Annapoorani Bala <Arunkumar>
2020-12-17 13:54:16 +0530

    Added the ajax method

--- qxf2_scheduler/reset_password.py
+++ qxf2_scheduler/reset_password.py
@@ -1,5 +1,5 @@
 from qxf2_scheduler import app
-from flask import render_template
+from flask import render_template, request
 from qxf2_scheduler import db
 
 from qxf2_scheduler.models import Login
@@ -7,7 +7,9 @@ from qxf2_scheduler.models import Login
 @app.route("/reset-password", methods=["GET","POST"])
 def reset_password():
     "Reset the password"
-    if request.method == "GET":
+    if request.method == 'GET':
         return render_template("reset-password.html")
-    if request.method == "POST":
+    if request.method == 'POST':
         email_id = request.form.get("emailid")
+        print(email_id)
+        return email_id

86b6caa3ca7b2626b74d8ff368f61fbefc19cde1
nilaya1234 <nilaya@qxf2.com>
2021-01-04 16:08:16 +0530

    mock test for total_count_list added

--- tests/test_select_interviewer.py
+++ tests/test_select_interviewer.py
@@ -5,6 +5,8 @@ from dateutil.parser import parse
 import os,sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from qxf2_scheduler.qxf2_scheduler import total_busy_slots
+from qxf2_scheduler.qxf2_scheduler import total_count_list
+
 #import os
 os.environ ["GOOGLE_APPLICATION_CREDENTIALS"] = r'C:\Users\Qxf2 Services\AppData\Roaming\gcloud\legacy_credentials\test@qxf2.com\adc.json'
 import logging
@@ -14,24 +16,34 @@ logging.getLogger('googleapicliet.discovery_cache').setLevel(logging.ERROR)
 
 class Total_Busy_Slots(unittest.TestCase):
 
-    date='12/22/2020'
+    date='1/5/2021'
     print("I am in 11")
 
     print("I am in 12")
 
-    @mock.patch.object(total_busy_slots,'attendee_email_id',return_value=['nilaya@qxf2.com',  'kavya.suryaprakash@qxf2.com'],create=True )
+    @mock.patch.object(total_busy_slots,'attendee_email_id',return_value=['nilaya@qxf2.com'],create=True )
 
 
     @mock.patch.object(total_busy_slots,'date',return_value='1/5/2021',create=True)
 
-    def test_total_busy_slots(self,mockdate,mockattendee_email_id,):
+    def test_total_busy_slots(self,mockdate,mockattendee_email_id):
 
         print("I am in 20",mockdate,mockattendee_email_id)
 
 
-        total_busy_time_list = [datetime.datetime(1900, 1, 1, 1, 40), datetime.datetime(1900, 1, 1, 1, 10)]
+        total_busy_time_list = [datetime.datetime(1900, 1, 1, 1, 30)]
         print("Failed")
 
         assert total_busy_slots(mockattendee_email_id.return_value,mockdate.return_value)== total_busy_time_list
+
+        print("pass")
+
+    @mock.patch.object(total_count_list,'attendee_email_id',return_value=['nilaya@qxf2.com'],create=True )
+
+    def test_total_count_list(self,mockattendee_email_id):
+
+        total_interview_count_list = [2]
+
+        assert total_count_list(mockattendee_email_id.return_value)== total_interview_count_list
 
         print("pass")

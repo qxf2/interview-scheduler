7f7849fbda264ae1c5a82502000ee2be0d518917
nilaya1234 <nilaya@qxf2.com>
2021-01-05 15:54:21 +0530

    mock test added for pick interviewer

--- tests/test_select_interviewer.py
+++ tests/test_select_interviewer.py
@@ -6,6 +6,7 @@ import os,sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from qxf2_scheduler.qxf2_scheduler import total_busy_slots
 from qxf2_scheduler.qxf2_scheduler import total_count_list
+from qxf2_scheduler.qxf2_scheduler import pick_interviewer
 
 #import os
 os.environ ["GOOGLE_APPLICATION_CREDENTIALS"] = r'C:\Users\Qxf2 Services\AppData\Roaming\gcloud\legacy_credentials\test@qxf2.com\adc.json'
@@ -20,7 +21,7 @@ class Total_Busy_Slots(unittest.TestCase):
     print("I am in 11")
 
     print("I am in 12")
-
+    '''
     @mock.patch.object(total_busy_slots,'attendee_email_id',return_value=['nilaya@qxf2.com'],create=True )
 
 
@@ -47,3 +48,12 @@ class Total_Busy_Slots(unittest.TestCase):
         assert total_count_list(mockattendee_email_id.return_value)== total_interview_count_list
 
         print("pass")
+    '''
+    @mock.patch.object(pick_interviewer,'attendee_email_id',return_value=['nilaya@qxf2.com','annpoorani@qxf2.com','rohan.j@qxf2.com','avinash@qxf2.com'],create=True )
+
+
+    @mock.patch.object(pick_interviewer,'date',return_value='1/5/2021',create=True)
+
+    def test_pick_interviewer(self,mockdate,mockattendee_email_id):
+
+        assert pick_interviewer(mockattendee_email_id.return_value,mockdate.return_value)=='rohan.j@qxf2.com'

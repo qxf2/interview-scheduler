2d550b9016ac43d6a22993b3c8afb5c62771be6e
nilaya1234 <nilaya@qxf2.com>
2021-02-03 20:51:09 +0530

    trying to change candidate name and mail id

--- QA/conf/api_example_conf.py
+++ QA/conf/api_example_conf.py
@@ -3,7 +3,7 @@
 job_details = {'role':'technical_man','interviewerlist':'["nilaya"]'}
 
 #get_candidate_details
-candidate_details = {'candidateName':'test_api_new123','candidateEmail':'test_api_new+567@gmail.com','jobApplied':'technical_man','addedcomments':'N/A'}
+candidate_details = {'candidateName':'test_api_new1234','candidateEmail':'test_api_new+5678@gmail.com','jobApplied':'technical_man','addedcomments':'N/A'}
 
 #get_interviewer_details
 interviewer_details = {'name':'test_nilaya','email':'nilaya+1234@qxf2.com','designation':'Senior QA','timeObject':'{"starttime":["10:00"],"endtime":["19:00"]}'}
@@ -11,4 +11,4 @@ interviewer_details = {'name':'test_nilaya','email':'nilaya+1234@qxf2.com','desi
 #authentication details
 user_name = 'nilaya123'
 password = 'nilaya1234'
-useremail = 'test+9000@qxf2.com'
+useremail = 'test+90000@qxf2.com'
